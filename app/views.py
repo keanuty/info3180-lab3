053789dfe3e9c614ac22d1b062560ed573f2ee5e
keanuty <keanutythompson@gmail.com>
2026-03-03 02:32:44 -0500

    Commited changes

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from app.forms import ContactForm
+
+from app import mail
+from flask_mail import Message
 
 
 ###
@@ -16,6 +20,32 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        msg = Message(
+            sender=("Website", "from@example.com"),
+            subject=form.subject.data,
+            recipients=["forallmytesting@outlook.com"]
+        )
+
+        msg.body = (
+            f"Name: {form.name.data}\n"
+            f"Email: {form.email.data}\n"
+            f"Subject: {form.subject.data}\n\n"
+            f"Message:\n{form.message.data}\n"
+        )
+
+        mail.send(msg)
+
+        flash("Your email was successfully sent.", "success")
+        return redirect(url_for("home"))
+
+    return render_template("contact.html", form=form)
 
 
 ###
